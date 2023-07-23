15b4b13b5b111d8b20cf64c06890d63ba171a8c9
Moran Habooba <moranhabooba@gmail.com>
2023-07-23 17:14:22 +0300

    first

---
+++ README.md
@@ -0,0 +1,43 @@
+# Moran's Portfolio Website
+
+This is my personal portfolio website showcasing some of the projects I've worked on as a Full Stack Developer. The website includes information about me, the technologies I've used, and a collection of projects that demonstrate my skills.
+
+## Description
+
+Welcome to my portfolio website! As a current student in Full Stack development, I am passionate about exploring new technologies and building quality web applications. This portfolio showcases some of my projects, each designed to demonstrate various skills and technologies.
+
+## Technologies Used
+
+The following technologies were used in developing this portfolio website:
+
+- HTML
+- CSS
+- Bootstrap
+- JavaScript
+- Sass
+- TypeScript
+- Api
+- OOP
+
+## Projects
+
+Here are some of the projects I've worked on:
+
+1. MP3 Player: A simple MP3 player built using HTML, CSS, and Bootstrap. [Try it out](https://moran-habooba.github.io/mp3-player2/)
+
+2. Budget App: An app to manage your budget using local storage, HTML5, CSS, and canvas. [Try it out](https://moran-habooba.github.io/Budget-App/)
+
+3. Memory Game: A memory game with an API, built using HTML5, CSS, and Bootstrap. [Try it out](https://moran-habooba.github.io/Memory-game-API/)
+
+4. Rock, Paper, Scissors: A classic rock-paper-scissors game using HTML5 and CSS. [Try it out](https://moran-habooba.github.io/game-Rock--Paper--Scissors/)
+
+5. Ping Pong Game: A fun and interactive ping pong game built with HTML5 canvas and JavaScript. [Try it out](https://moran-habooba.github.io/Ping-Pong-Game/)
+
+6. Drawing App: A web-based drawing application that allows users to create digital art using various drawing tools. [Try it out](https://moran-habooba.github.io/Drawing-App/)
+
+These are just some of the projects, you can see the rest on the website 😊
+
+## Contact
+
+Feel free to contact me anytime if you have any questions, comments or suggestions.
+You can contact me via the following email: moranhabooba@gmail.com .
---
+++ script.js
@@ -0,0 +1,73 @@
+const touchButton = document.querySelector(".float-text");
+const card = document.querySelector(".float-card-info");
+const close = document.querySelector(".close");
+
+touchButton.addEventListener("click", moveCard);
+close.addEventListener("click", moveCard);
+
+function moveCard() {
+  card.classList.toggle("active");
+}
+
+function toggleDarkMode() {
+  var body = document.body;
+  body.classList.toggle("dark-mode");
+}
+
+// *****************
+
+document.addEventListener("DOMContentLoaded", function () {
+  const projectsPerLoad = 4;
+
+  // Get references to the project container and the "Load More" button
+  // const projectContainer = document.querySelector(".container");
+  const loadMoreButton = document.getElementById("loadMoreButton");
+
+  // Get all the project cards
+  const projectCards = document.querySelectorAll(".card");
+
+  // Variable to keep track of the number of projects already shown
+  let projectsShown = 0;
+
+  // Function to load more projects when the "Load More" button is clicked
+  function loadMoreProjects() {
+    // Calculate the number of projects to show in this batch
+    const projectsToShow = Math.min(
+      projectsPerLoad,
+      projectCards.length - projectsShown
+    );
+
+    // Show the next batch of projects
+    for (let i = projectsShown; i < projectsShown + projectsToShow; i++) {
+      const projectCard = projectCards[i];
+      projectCard.style.display = "block";
+    }
+
+    // Update the number of projects already shown
+    projectsShown += projectsToShow;
+
+    // If all projects are shown, hide the "Load More" button
+    if (projectsShown === projectCards.length) {
+      loadMoreButton.style.display = "none";
+    }
+  }
+
+  // Function to hide all projects
+  function hideAllProjects() {
+    projectCards.forEach((projectCard) => {
+      projectCard.style.display = "none";
+    });
+  }
+
+  // Show the first batch of projects (4 projects)
+  hideAllProjects();
+  loadMoreProjects();
+
+  // Attach the click event to the "Load More" button
+  loadMoreButton.addEventListener("click", loadMoreProjects);
+});
+
+document.addEventListener("selectstart", function (e) {
+  e.preventDefault();
+  // alert('העתקה אסורה!');
+});
